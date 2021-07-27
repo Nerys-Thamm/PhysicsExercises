928b4a256b9ef8ed5ce70a6702ee992cc461fc8f
Nerys-Thamm <nerysthamm@gmail.com>
2021-07-27 18:15:47 +1200

    Did some of the first exercise

--- PhysicsExercises/PhysicsExercises.cpp
+++ PhysicsExercises/PhysicsExercises.cpp
@@ -2,10 +2,47 @@
 //
 
 #include <iostream>
+#include "CVector.h"
+#include <vector>
+
+void LagrangeTest()
+{
+	std::vector<CVector::Vector3> Aexamples = { CVector::Vector3{1.0f,2.0f,3.0f} };
+	std::vector<CVector::Vector3> Bexamples = { CVector::Vector3{2.0f,3.0f,1.0f} };
+	std::vector<CVector::Vector3> Cexamples = { CVector::Vector3{3.0f,1.0f,2.0f} };
+
+	if (Aexamples.size() != Bexamples.size() || Bexamples.size() != Cexamples.size())
+	{
+		std::cout << "Examples not the same size!" << std::endl;
+		return;
+	}
+
+	for (int i = 0; i < Aexamples.size(); i++)
+	{
+		std::cout << "Vector A: " << CVector::Stringify(Aexamples[i]) << std::endl;
+		std::cout << "Vector B: " << CVector::Stringify(Bexamples[i]) << std::endl;
+		std::cout << "Vector C: " << CVector::Stringify(Cexamples[i]) << std::endl;
+
+		std::cout << "Formula:  A x (B x C) = (A DOT C)B -(A DOT B)C" << std::endl;
+
+		CVector::Vector3 lhs = CVector::CrossProduct(Aexamples[i], CVector::CrossProduct(Bexamples[i], Cexamples[i]));
+
+		std::cout << "A x (B x C) = " << CVector::Stringify(lhs) << std::endl;
+
+		CVector::Vector3 rhs = (Bexamples[i] * CVector::Dot(Aexamples[i], Cexamples[i])) - (Cexamples[i] * CVector::Dot(Aexamples[i], Bexamples[i]));
+
+		std::cout << "(A DOT C)B -(A DOT B)C = " << CVector::Stringify(rhs) << std::endl;
+
+
+
+		system("pause");
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	LagrangeTest();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
